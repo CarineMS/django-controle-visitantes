ab5fce37de857b8c1d2847d62948e8ede884481d
CarineMS <carinemadsoares@gmail.com>
2025-07-05 19:42:47 -0300

    :zap: Renderizando as urls 'informações visitantes, 'registrar visitantes' e 'início' com o Django

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -8,3 +8,19 @@ class VisitanteForm(forms.ModelForm):
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo"
         ]
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro"
+            },
+            "cpf": {
+                "required": "O cpf do visitante é obrigatório para o registro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatória para o registro",
+                "invalid": "Por favor, informe o formayo válido para a data de nascimento (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "O número da casa é obrigatório para o registro"
+            },
+
+        }
