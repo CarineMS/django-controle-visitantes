b48f2c65db475a51296449343e607ff9847c17fc
CarineMS <carinemadsoares@gmail.com>
2025-07-06 18:33:05 -0300

    :sparkles: Criação da página de login

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from apps.dashboard.views import index
 from apps.visitantes.views import (
     registrar_visitante, informacoes_visitante, finalizar_visita
@@ -8,6 +10,14 @@ from apps.visitantes.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html"
+        ),
+        name="login"
+    ),
 
     path(
         "",
