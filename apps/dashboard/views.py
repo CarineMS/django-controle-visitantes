78d48e0cdf0396c314c1804b764ebb204893b0d5
CarineMS <carinemadsoares@gmail.com>
2025-07-06 18:09:09 -0300

    :zap: Criação da metrica 'visitantes mês'

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from visitantes.models import Visitante
 
+from django.utils import timezone
+
 def index(request):
 
     todos_visitantes = Visitante.objects.all()
@@ -17,12 +19,21 @@ def index(request):
         status="FINALIZADO"
     )
 
+    current_date = timezone.now()
+    current_month = current_date.month
+
+    visitantes_mes = todos_visitantes.filter(
+        horario_chegada__month=current_month
+    )
+
+
     context = {
         "nome_pagina" : "Início do dashboard",
         "todos_visitantes" : todos_visitantes,
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizado": visitantes_finalizado.count(),
+        "visitantes_mes": visitantes_mes.count(),
     }
 
     return render(request, "index.html", context)
